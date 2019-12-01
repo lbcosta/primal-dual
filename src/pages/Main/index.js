1892f220d82d4cbafcda1e7096bdb0ed8724cccf
Leonardo Costa <leocosta@edu.unifor.br>
2019-12-01 12:33:19 -0300

    Inserting inputs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,94 @@
 import React, { useState } from "react";
+import Select from "react-select";
 
-import { PageLogo, PageWrapper, Container } from "./styles";
+import {
+  PageLogo,
+  PageWrapper,
+  PageContent,
+  Container,
+  StaticSide,
+  DynamicSide,
+  Equation,
+  Variable
+} from "./styles";
 import Logo from "../../assets/img/Logo.png";
 
+const objectiveOptions = [
+  {
+    value: "min",
+    label: "MIN"
+  },
+  {
+    value: "max",
+    label: "MAX"
+  }
+];
+
 export default function Main() {
-  const [objective, setObjective] = useState("min");
-  const [objFunction, setObjFunction] = useState([]);
-  const [constraints, setConstraints] = useState([]);
+  const [objective, setObjective] = useState(objectiveOptions[0]);
+  const [objFunction, setObjFunction] = useState([6, 4]);
+  const [constraints, setConstraints] = useState([
+    [1, 2],
+    [3, 4]
+  ]);
   const [equalityConstraints, setEqualityConstraints] = useState([]);
+
+  function buttonOrSpan(actualIdx, arr) {
+    return actualIdx === arr.length - 1 ? (
+      <button type="button">+</button>
+    ) : (
+      <span>+</span>
+    );
+  }
 
   return (
     <PageWrapper>
       <PageLogo src={Logo} />
-      <Container>
-        <p>Min Z = 2X1 + 3X2 [+]</p>
-        <p>S.T. 4X1 >= 8</p>
-        <p>+</p>
-        <p>X1 > 0, X2 </p>
-      </Container>
+      <PageContent>
+        <Container>
+          <StaticSide>
+            <div>
+              <Select
+                value={objective}
+                onChange={setObjective}
+                options={objectiveOptions}
+              />
+              <span>Z =</span>
+            </div>
+            <div>
+              <p>S.T.</p>
+            </div>
+          </StaticSide>
+          <DynamicSide>
+            <Equation>
+              {objFunction.map((variable, idx) => (
+                <Variable>
+                  <input type="number" value={variable} />
+                  <span>
+                    X <sub>{idx + 1}</sub>{" "}
+                  </span>
+                  {buttonOrSpan(idx, objFunction)}
+                </Variable>
+              ))}
+            </Equation>
+            {constraints.map(constraint => (
+              <Equation>
+                {constraint.map((variable, idx) => (
+                  <Variable>
+                    <input type="number" value={variable} />
+                    <span>
+                      X <sub>{idx + 1}</sub>{" "}
+                    </span>
+                    {buttonOrSpan(idx, constraint)}
+                  </Variable>
+                ))}
+              </Equation>
+            ))}
+
+            <button type="button">+</button>
+          </DynamicSide>
+        </Container>
+      </PageContent>
     </PageWrapper>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -20,16 +20,48 @@ export const PageWrapper = styled.div`
   justify-content: space-around;
 `;
 
-export const Container = styled.div`
+export const PageContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   height: 80%;
   width: 80%;
   border-radius: 10px;
+
   background-color: #fff;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-
   :hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   }
 `;
+
+export const Container = styled.div`
+  display: flex;
+  border: 1px red solid;
+
+  &,
+  & > div {
+    min-width: 300px;
+  }
+`;
+
+export const StaticSide = styled.div`
+  & > div {
+    display: flex;
+    align-items: center;
+    > div {
+      min-width: 100px;
+    }
+  }
+`;
+
+export const DynamicSide = styled.div``;
+
+export const Equation = styled.ul`
+  display: flex;
+  min-height: 38px;
+`;
+
+export const Variable = styled.li``;
